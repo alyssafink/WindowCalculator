c23cec1fecd7cb4e2bd37b9470633afb33cb2c6c
Alyssa Fink <alyssafink@arizona.edu>
2024-03-11 21:51:51 -0700

    Baseline part 2: featuring components

--- window-calculator/src/app/app.routes.ts
+++ window-calculator/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+import { ReportComponent } from './report/report.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'report', component: ReportComponent},
+    {path: 'home', component: HomeComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
